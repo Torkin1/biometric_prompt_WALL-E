5024b5f598b2269f81964424dba72c6ff27fafdb
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-09-22 16:44:14 +0200

    #12

--- FakeBank/app/src/main/java/com/mihaijianu/dmlbank/banktransfer/BankTransferController.java
+++ FakeBank/app/src/main/java/com/mihaijianu/dmlbank/banktransfer/BankTransferController.java
@@ -4,6 +4,15 @@ import com.mihaijianu.dmlbank.entities.Account;
 
 public class BankTransferController {
 
+    // Singleton class
+    private static BankTransferController ref;
+    public static BankTransferController getReference(){
+        if (ref == null){
+            ref = new BankTransferController();
+        }
+        return ref;
+    }
+
     public void pay(Account payerAccount, int toPay) throws InsufficentBalanceException {
 
         // checks if payerAccount has enough money to pay transaction
