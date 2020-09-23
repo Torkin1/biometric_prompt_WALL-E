7a036cd173fb8057aec1db442a68883c7f026544
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-09-23 17:05:11 +0200

    #15

--- FakeBank/app/src/main/java/com/mihaijianu/dmlbank/TransferActivity.java
+++ FakeBank/app/src/main/java/com/mihaijianu/dmlbank/TransferActivity.java
@@ -1,148 +1,150 @@
 package com.mihaijianu.dmlbank;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 
+import android.app.KeyguardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
-
-import java.util.concurrent.Executor;
+import com.mihaijianu.dmlbank.banktransfer.BankTransferController;
+import com.mihaijianu.dmlbank.banktransfer.InsufficentBalanceException;
+import com.mihaijianu.dmlbank.entities.Account;
 
 public class TransferActivity extends AppCompatActivity {
-    private TextInputLayout textInputDescription;
-    private TextInputLayout textInputSwiftCode;
-    private TextInputLayout textInputIBAN;
-    private TextInputLayout textInputAmount;
-    public double amount = 0;
 
+    // onClick listener for bTransfer
+    private class onBTransferClickListener implements View.OnClickListener{
+
+        // Callback for biometricPrompt used by onBTransferClick.onClick
+        private class BiometricPromptCallback extends BiometricPrompt.AuthenticationCallback {
+
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                Toast.makeText(TransferActivity.this, String.format(getString(R.string.TRANSFER_FAILED), errString, errorCode), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                // does the transfer
+                try {
+                    BankTransferController.getReference().pay(Account.getReference(),
+                            Integer.parseInt(TransferActivity
+                                    .this
+                                    .holder
+                                    .textInputAmount
+                                    .getEditText()
+                                    .getText()
+                                    .toString()
+                            )
+                    );
+                    Toast.makeText(TransferActivity.this, R.string.TRANSFER_SUCCESS, Toast.LENGTH_SHORT).show();
+                } catch (InsufficentBalanceException e) {
+                    Toast.makeText(TransferActivity.this, R.string.TRANSFER_FAILED_INSUFFICIENT_FUNDS, Toast.LENGTH_LONG).show();
+                } catch (NullPointerException | NumberFormatException e) {
+                    Toast.makeText(TransferActivity.this, R.string.TRANSFER_FAILED_CANT_READ_AMOUNT_FROM_GUI, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                Toast.makeText(TransferActivity.this, R.string.TRANSFER_FAILED_AUTH_FAILED, Toast.LENGTH_LONG).show();
+            }
+        }
+
+        // onClick
+        @Override
+        public void onClick(View v) {
+
+            // checks if device is secured via non-biometric authentication errors
+            boolean isNonBiometricAuthAvailable;
+            try {
+                isNonBiometricAuthAvailable = ((KeyguardManager) TransferActivity.this.getSystemService(Context.KEYGUARD_SERVICE)).isDeviceSecure();
+            } catch (NullPointerException e){
+                Toast.makeText(TransferActivity.this, R.string.FAILED_TO_CHECK_NON_BIOMETRIC_AUTH, Toast.LENGTH_LONG).show();
+                isNonBiometricAuthAvailable = false;
+            }
+
+            // checks if biometric input is available
+            switch (TransferActivity.this.biometricManager.canAuthenticate()) {//userÃ² una costate per vedere le diverse possibilitÃ 
+                case BiometricManager.BIOMETRIC_SUCCESS: {
+                    break;
+                }
+                // if biometric hw is not present or unavailable, payment should be enabled only if other methods of auth are available
+                case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:{
+                    if (!isNonBiometricAuthAvailable) {
+                        // No auth methods available, disable transfers
+                        (new AlertDialog.Builder(TransferActivity.this))
+                                .setTitle(R.string.NEITHER_BIOMETRIC_NOR_NONBIOMETRIC_ALERT_TITLE)
+                                .setMessage(R.string.NEITHER_BIOMETRIC_NOR_NONBIOMETRIC_ALERT_MSG)
+                                .create()
+                                .show();
+                        return;
+                    }
+                    break;
+                }
+                case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED: {
+                    // Prompts user to enroll a biometric model
+                    (new AlertDialog.Builder(TransferActivity.this))
+                            .setTitle(R.string.NO_BIOMETRIC_ENROLLED_ALERT_TITLE)
+                            .setMessage(R.string.NO_BIOMETRIC_ENROLLED_ALERT_MSG)
+                            .create()
+                            .show();
+                    // Starts activity for biometric model enrollment
+                    Intent enrollIntent = new Intent(Settings.ACTION_FINGERPRINT_ENROLL);
+                    startActivity(enrollIntent);
+                    break;
+                }
+            }
+
+            // asks for biometric input
+            BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                    .setTitle(getString(R.string.BIOMETRIC_PROMPT_TITLE))
+                    .setSubtitle(getString(R.string.BIOMETRIC_PROMPT_SUBTITLE))
+                    .setDeviceCredentialAllowed(isNonBiometricAuthAvailable)   // enables user to authenticate using other auth methods (PIN, password, pattern, ...) if there are any enrolled. Please note that you can't call setNegativeButtonText along with this method
+                    .build();
+            BiometricPrompt biometricPrompt = new BiometricPrompt(TransferActivity.this,
+                    ContextCompat.getMainExecutor(TransferActivity.this),
+                    new BiometricPromptCallback());
+            biometricPrompt.authenticate(promptInfo);
+        }
+    }
+
+    // Holder
+    private class Holder{
+        private TextInputLayout textInputAmount;
+        private Button bTransfer;
+
+        private Holder() {
+            this.textInputAmount = findViewById(R.id.text_input_amount);
+            this.bTransfer.setOnClickListener(new onBTransferClickListener());
+        }
+
+    }
+
+    // activity
+    public double amount = 0;
+    private BiometricManager biometricManager;
+    private Holder holder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // sets up activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer);
-
-        textInputDescription = findViewById(R.id.text_input_description);
-        textInputSwiftCode = findViewById(R.id.text_input_swiftCode);
-        textInputIBAN = findViewById(R.id.text_input_IBAN);
-        textInputAmount = findViewById(R.id.text_input_amount);
-
-
-
-        //biometric mangaer, controllo se puÃ² usare o meno il biometric
-        BiometricManager biometricManager = BiometricManager.from(this);
-        switch (biometricManager.canAuthenticate()) {//userÃ² una costate per vedere le diverse possibilitÃ 
-            case BiometricManager.BIOMETRIC_SUCCESS: //successo, permesso fornito
-                Toast.makeText(getApplicationContext(), "Biometric request approved, click again to access", Toast.LENGTH_LONG).show();
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE://no hardware
-                Toast.makeText(getApplicationContext(), "No Hardware", Toast.LENGTH_LONG).show();
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE://no hardware disponibile
-                Toast.makeText(getApplicationContext(), "Hardware unvailable", Toast.LENGTH_LONG).show();
-                //Todo bottone che varia
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED://no hardware
-                Toast.makeText(getApplicationContext(), "No fingerPrints saved", Toast.LENGTH_LONG).show();
-                break;
-        }
-
-        //biometric dialog box
-        //creazione executer
-        Executor executor = ContextCompat.getMainExecutor(this);
-
-        //biometricPrompt
-        final BiometricPrompt biometricPrompt = new BiometricPrompt(TransferActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
-            @Override//chiamato quando ci sono problemi nell'autenticazione
-            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
-                super.onAuthenticationError(errorCode, errString);
-            }
-
-            @Override//chiamato se ho un successo con l'auttenticazione
-            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
-                super.onAuthenticationSucceeded(result);
-                Toast.makeText(getApplicationContext(), "fingerprint accepted", Toast.LENGTH_LONG).show();
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-
-            }
-
-            @Override//chiamato se se ho fallito l'autenticazione
-            public void onAuthenticationFailed() {
-                // TODO: payment fails, return to main menu
-                super.onAuthenticationFailed();
-            }
-        });
-
-        //Creo il biometric Dialog
-        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
-                .setTitle("PayConfirmation")
-                .setDescription("Requestd fingerprint to acquire google, a total of 20 bilions")
-                .setNegativeButtonText("Cancel")
-                .build();
-
-        findViewById(R.id.transfer_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!validateDescription() | !validateSwiftCode() | !validateIBAN())
-                    return;
-                String temp = textInputAmount.getEditText().getText().toString();
-                amount = Integer.parseInt(temp);
-                biometricPrompt.authenticate(promptInfo);
-
-            }
-        });
-
-    }
-
-    private boolean validateDescription() {
-        String descriptionInput = textInputDescription.getEditText().getText().toString().trim();
-        if (descriptionInput.isEmpty()) {
-            textInputDescription.setError("Field can't be empty");
-            return false;
-        } else if (descriptionInput.length() > 50) {
-            textInputSwiftCode.setError("Description to long too long");
-            return false;
-        }
-        else {
-            textInputDescription.setError(null);
-//            textInputEmail.setErrorEnabled(true);
-            return true;
-        }
-    }
-
-    private boolean validateSwiftCode() {
-        String swiftCodeInput = textInputSwiftCode.getEditText().getText().toString().trim();
-        if (swiftCodeInput.isEmpty()) {
-            textInputSwiftCode.setError("Field can't be empty");
-            return false;
-        } else if (swiftCodeInput.length() > 12) {
-            textInputSwiftCode.setError("SwiftCode to long too long");
-            return false;
-        } else {
-            textInputSwiftCode.setError(null);
-            return true;
-        }
-
-    }
-
-    private boolean validateIBAN() {
-        String IBANInput = textInputIBAN.getEditText().getText().toString().trim();
-        if (IBANInput.isEmpty()) {
-            textInputIBAN.setError("Field can't be empty");
-            return false;
-        } else if (IBANInput.length() > 27) {
-            textInputIBAN.setError("Username too long");
-            return false;
-        } else {
-            textInputIBAN.setError(null);
-            return true;
-        }
-
+        this.biometricManager = BiometricManager.from(this);
+        Holder holder = new Holder();
     }
 }
